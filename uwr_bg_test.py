13b6aad4a7e28dc0a0177bf2f66660b134820397
linusduerr <duerr.linus@gmail.com>
2024-02-06 18:12:56 +0100

    Refined algorithm slightly to detect pauses

--- uwr_bg_test.py
+++ uwr_bg_test.py
@@ -2,14 +2,17 @@ from __future__ import print_function
 import cv2 as cv
 import argparse
 import numpy as np
-parser = argparse.ArgumentParser(description='This program shows how to use background subtraction methods provided by \
-                                              OpenCV. You can process both videos and images.')
+
+INPUT_PATH = 'input/'
+
+parser = argparse.ArgumentParser(description='This program takes two input video streams of a UWR game and switches between the angles automatically')
 parser.add_argument('--L', type=str, help='Filename of left clips without extension', default='L')
 parser.add_argument('--R', type=str, help='Filename of right clips without extension', default='R')
 parser.add_argument('--ext', type=str, help='File extension for video files', default='.mp4')
 parser.add_argument('--clipsL', type=str, help='Number of clips left video is divided into', default='1')
 parser.add_argument('--clipsR', type=str, help='Number of clips right video is divided into', default='1')
 parser.add_argument('--algo', type=str, help='Background subtraction method (KNN, MOG2, GSOC).', default='MOG2')
+parser.add_argument('-pauses', action='store_true', help='If this argument is given, the program will attempt to cut game pauses out')
 args = parser.parse_args()
 
 cv.namedWindow('Frame', cv.WINDOW_NORMAL)
@@ -40,6 +43,7 @@ elif args.algo == 'GSOC':
 
 clipsL = int(args.clipsL)
 clipsR = int(args.clipsR)
+
 if (clipsL == 1):
     lCapName = args.L + args.ext
     rCapName = args.R + args.ext
@@ -52,13 +56,13 @@ curClipR = 1
 learningRateL = 0.0002
 learningRateR = 0.0002
 
-lCap = cv.VideoCapture(cv.samples.findFileOrKeep(lCapName))
-rCap = cv.VideoCapture(cv.samples.findFileOrKeep(rCapName))
+lCap = cv.VideoCapture(cv.samples.findFileOrKeep(INPUT_PATH + lCapName))
+rCap = cv.VideoCapture(cv.samples.findFileOrKeep(INPUT_PATH + rCapName))
 
 writer = cv.VideoWriter('out.mp4', cv.VideoWriter_fourcc(*'mp4v'), 30, (1920, 1080))
 
-lCap.set(1, 27000)
-rCap.set(1, 27000)
+lCap.set(1, 27500)
+rCap.set(1, 27500)
 
 if not lCap.isOpened():
     print('Unable to open: ' + args.L)
@@ -66,6 +70,18 @@ if not lCap.isOpened():
 if not rCap.isOpened():
     print('Unable to open: ' + args.R)
     exit(0)
+def is_game_paused(lMask, rMask):
+    lowerHalfMeanR = rMask[rMask.shape[0]//2 :, :].mean()
+    lowerHalfMeanL = lMask[lMask.shape[0]//2 :, :].mean()
+    upperHalfMeanL = rMask[: rMask.shape[0]//2, :].mean()
+    upperHalfMeanR = lMask[: lMask.shape[0]//2, :].mean()
+    return lowerHalfMeanL < 1 and lowerHalfMeanR < 1 and upperHalfMeanL < 40 and upperHalfMeanR < 40
+
+def score(mask):
+    lowerHalfMean = mask[mask.shape[0]//2 :, :].mean()
+    upperHalfMean = mask[: mask.shape[0]//2, :].mean()
+    return 3*lowerHalfMean + upperHalfMean
+
 while True:
     ret, lFrameOrig = lCap.read()
     ret, rFrameOrig = rCap.read()
@@ -75,7 +91,7 @@ while True:
             curClipL += 1
             lCapName = args.L + str(curClipL) + args.ext
             print("Reading: ", lCapName)
-            lCap = cv.VideoCapture(cv.samples.findFileOrKeep(lCapName))
+            lCap = cv.VideoCapture(cv.samples.findFileOrKeep(INPUT_PATH + lCapName))
             if not lCap.isOpened():
                 print('Unable to open: ' + args.L)
                 exit(0)
@@ -91,7 +107,7 @@ while True:
             curClipR += 1
             rCapName = args.R + str(curClipR) + args.ext
             print("Reading: ", rCapName)
-            rCap = cv.VideoCapture(cv.samples.findFileOrKeep(rCapName))
+            rCap = cv.VideoCapture(cv.samples.findFileOrKeep(INPUT_PATH + rCapName))
             if not rCap.isOpened():
                 print('Unable to open: ' + args.R)
                 exit(0)
@@ -111,9 +127,9 @@ while True:
     lBG = lBackSub.getBackgroundImage()
     rBG = rBackSub.getBackgroundImage()
 
-    if lMask.mean() < 7 and rMask.mean() < 7:
+    if args.pauses and is_game_paused(lMask, rMask):
         frame = np.zeros(lFrameOrig.shape)
-    elif lMask.mean() > rMask.mean():
+    elif score(lMask) > score(rMask):
         frame = lFrameOrig
         writer.write(frame)
     else:
@@ -137,6 +153,6 @@ while True:
         lCap.set(1, lframeNum + 30)
         rCap.set(1, rframeNum + 30)
     elif keyboard == 108:
-        learningRateL = 1
+        learningRateL = 0
     elif keyboard == 114:
-        learningRateR = 1
+        learningRateR = 0
